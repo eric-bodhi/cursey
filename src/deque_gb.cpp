d33db55fdf0e3656a2b3bb978c5220f70aa6bade
Eric Floyd <erixefb@gmail.com>
2025-01-04 21:15:06 -0500

    new gap buffer put in source code, still need to change the texteditor class along with it

---
+++ src/deque_gb.cpp
@@ -0,0 +1,78 @@
+#include "deque_gb.h"
+#include <deque>
+#include <string>
+#include <stdexcept>
+
+Gb::Gb(const std::string& string, const size_t& cursor) {
+
+}
+
+void Gb::move_cursor(size_t index) {
+    // if cursor in same position
+    if (index == left.size()) {
+        return;
+    }
+
+    if (index < left.size()) {
+        while (left.size() != index) {
+            move_left();
+        }
+    }
+
+    else {
+        while (left.size() != index) {
+            move_right();
+        }
+    }
+}
+
+// (in)(de)crement cursor
+void Gb::move_left() {
+    if (left.size() == 0) {
+        throw std::runtime_error("move left: out of range");
+    }
+
+    right.push_front(left.back());
+    left.pop_back();
+}
+
+void Gb::move_right() {
+    if (right.size() == 0) {
+        throw std::runtime_error("move right: out of range");
+    }
+
+    left.push_back(right.front());
+    right.pop_front();
+}
+
+void Gb::insert(const char c) {
+    left.push_back(c);
+}
+
+void Gb::del() {
+    left.pop_back();
+}
+
+std::string Gb::string_with_gap() {
+    return std::string(left.begin(), left.end()) + " " + std::string(right.begin(), right.end());
+}
+
+std::string Gb::string_with_gap() const {
+    return std::string(left.begin(), left.end()) + " " + std::string(right.begin(), right.end());
+}
+
+std::string Gb::to_string() {
+    return std::string(left.begin(), left.end()) + std::string(right.begin(), right.end());
+}
+
+std::string Gb::to_string() const {
+    return std::string(left.begin(), left.end()) + std::string(right.begin(), right.end());
+}
+
+size_t Gb::size() {
+    return left.size() + right.size();
+}
+
+size_t Gb::size() const {
+    return left.size() + right.size();
+}
---
+++ src/deque_gb.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <deque>
+#include <string>
+
+class Gb {
+private:
+    std::deque<char> left;
+    std::deque<char> right;
+
+public:
+    Gb(const std::string& string = "", const size_t& cursor = 0);
+
+    void move_cursor(size_t index);
+    void move_left();
+    void move_right();
+    void insert(const char c);
+    void del();
+
+    std::string string_with_gap();
+    std::string string_with_gap() const;
+
+    std::string to_string();
+    std::string to_string() const;
+
+    size_t size();
+    size_t size() const;
+};
