54ed30b5443f341cf0eddbb07db9c8ae15b3a174
Eric Floyd <erixefb@gmail.com>
2025-02-06 21:13:58 -0500

    Fixed Up/Down negative overflow bug

--- src/core/cursor.cpp
+++ src/core/cursor.cpp
@@ -11,7 +11,7 @@ void CursorManager::moveDir(Direction direction) {
         if (m_cursor.row > 0) {
             if (m_buffer.getLineLength(m_cursor.row - 1) <=
                 m_cursor.original_col) {
-                m_cursor.col = m_buffer.getLineLength(m_cursor.row - 1) - 1;
+                m_cursor.col = m_buffer.getLineLength(m_cursor.row - 1);
             } else if (m_buffer.getLineLength(m_cursor.row - 1) >=
                        m_cursor.original_col) {
                 m_cursor.col = m_cursor.original_col;
@@ -24,7 +24,7 @@ void CursorManager::moveDir(Direction direction) {
         if (m_cursor.row < m_buffer.lineCount() - 1) {
             if (m_buffer.getLineLength(m_cursor.row + 1) <=
                 m_cursor.original_col) {
-                m_cursor.col = m_buffer.getLineLength(m_cursor.row + 1) - 1;
+                m_cursor.col = m_buffer.getLineLength(m_cursor.row + 1);
             } else if (m_buffer.getLineLength(m_cursor.row + 1) >=
                        m_cursor.original_col) {
                 m_cursor.col = m_cursor.original_col;
